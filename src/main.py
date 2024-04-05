7157a7a64e736e552b96f068eb9e65919221ba9c
Alex Jose <alexjose4u@gmail.com>
2024-04-05 21:08:07 +0530

    update

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ client = AzureOpenAI(
 
   azure_endpoint = "https://d-analytics-eus-oai-chatbots.openai.azure.com/", 
 
-  api_key=os.getenv("AZURE_OPENAI_KEY"),  
+  api_key=os.getenv("INPUT_OPENAI_API_KEY"),  
 
   api_version="2024-02-15-preview"
 
