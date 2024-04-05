abefd601ff6d0b84211b975915620b9fe0171225
Alex Jose <alexjose4u@gmail.com>
2024-04-05 20:58:40 +0530

    update

--- src/main.py
+++ src/main.py
@@ -1,5 +1,39 @@
 import os
+from openai import AzureOpenAI
+
+client = AzureOpenAI(
+
+  azure_endpoint = "https://d-analytics-eus-oai-chatbots.openai.azure.com/", 
+
+  api_key=os.getenv("AZURE_OPENAI_KEY"),  
+
+  api_version="2024-02-15-preview"
+
+)
 
 # Read env variable
 env_var = os.environ.get('INPUT_WHO-TO-GREET')
 print('Hello ' + env_var)
+
+message_text = [{"role":"system","content":"You are an AI assistant that helps people find information."}]
+
+
+completion = client.chat.completions.create(
+
+  model="chatbot-innovation-india", # model = "deployment_name"
+
+  messages = message_text,
+
+  temperature=0.7,
+
+  max_tokens=800,
+
+  top_p=0.95,
+
+  frequency_penalty=0,
+
+  presence_penalty=0,
+
+  stop=None
+
+)
